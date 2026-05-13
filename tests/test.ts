b02cb751459d8346e98d666c8ad319e80ba90657
Stas Negriy <negriystas@gmail.com>
2026-05-13 10:42:20 +0300

    Initial commit

---
+++ tests/test.ts
@@ -0,0 +1,19 @@
+import { group, check } from 'k6';
+import http from 'k6/http';
+
+// SWAGGER "PET STORE" API - https://petstore.swagger.io/#/
+
+export const options = {
+  vus: 1,
+  iterations: 1,
+};
+
+export default function() {
+  group('Default group', function () {
+   
+  const resp = http.get('https://petstore.swagger.io/v2/pet/findByStatus?status=available');
+    console.log(resp.body);
+
+    check(resp, { 'status equals 200': (r) => r.status === 200 });
+  });
+}
